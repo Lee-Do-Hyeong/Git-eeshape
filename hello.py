b4bded799f1fa9a77ec5b1b7ef66f905362b1412
eeshape <eeshape@inu.ac.kr>
2024-04-06 22:04:09 +0900

    test

--- hello.py
+++ hello.py
@@ -1,1 +1,18 @@
-print('hello')
+hour, min = input().split()    
+hour = int(hour)    
+min = int(min) 
+
+time = input()
+time = int(time) 
+
+hour += time//60
+min += time%60
+
+if min>=60:
+    hour+=1
+    min-=60
+
+if hour>=24:
+    hour -=24
+
+print(hour,min)
